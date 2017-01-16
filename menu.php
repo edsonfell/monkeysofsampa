542dfe68b3981f19c23ef94c81ef66ccd207bdf5
William Queiroz <williamdenovaisqueiroz@outlook.com>
2017-01-16 21:59:57 -0200

    .htacces-url-amigavel

--- menu.php
+++ menu.php
@@ -9,20 +9,20 @@ if ($pagina == "index") { ?>
     <div id="menu-top">
         <nav>
             <div class="nav-wrapper z-depth-1">
-                <a href="index.php" class="brand-logo center"><img class="logo-menu" src="img/logo-bar.png"></a>
+                <a href="index" class="brand-logo center"><img class="logo-menu" src="img/logo-bar.png"></a>
                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
 
                 <ul class="menu right hide-on-med-and-down">
-                    <li><a href="pages/aulas.php" class="thin"><i class="icon-menu material-icons">perm_identity</i>Aulas</a></li>
-                    <li><a href="pages/midia.php" class="thin"><i class="icon-menu material-icons">play_circle_filled</i>Mídia</a></li>
-                    <li><a href="pages/projetos.php" class="thin"><i class="icon-menu material-icons">work</i>Projetos</a></li>
+                    <li><a href="pages/aulas" class="thin"><i class="icon-menu material-icons">perm_identity</i>Aulas</a></li>
+                    <li><a href="pages/midia" class="thin"><i class="icon-menu material-icons">play_circle_filled</i>Mídia</a></li>
+                    <li><a href="pages/projetos" class="thin"><i class="icon-menu material-icons">work</i>Projetos</a></li>
                 </ul>
 
                 <ul class="menu side-nav" id="mobile-demo">
-                    <li><a href="index.php" class="thin white-text"><i class="icon-menu material-icons">store</i>Início</a></li>
-                    <li><a href="pages/aulas.php" class="thin white-text"><i class="icon-menu material-icons">perm_identity</i>Aulas</a></li>
-                    <li><a href="pages/midia.php" class="thin white-text"><i class="icon-menu material-icons">play_circle_filled</i>Mídia</a></li>
-                    <li><a href="pages/projetos.php" class="thin white-text"><i class="icon-menu material-icons">work</i>Projetos</a></li>
+                    <li><a href="index" class="thin white-text"><i class="icon-menu material-icons">store</i>Início</a></li>
+                    <li><a href="pages/aulas" class="thin white-text"><i class="icon-menu material-icons">perm_identity</i>Aulas</a></li>
+                    <li><a href="pages/midia" class="thin white-text"><i class="icon-menu material-icons">play_circle_filled</i>Mídia</a></li>
+                    <li><a href="pages/projetos" class="thin white-text"><i class="icon-menu material-icons">work</i>Projetos</a></li>
                 </ul>
 
             </div>
@@ -33,20 +33,20 @@ if ($pagina == "index") { ?>
     <div id="menu-top">
         <nav>
             <div class="nav-wrapper z-depth-1">
-                <a href="../index.php" class="brand-logo center"><img class="logo-menu" src="../img/logo-bar.png"></a>
+                <a href="../index" class="brand-logo center"><img class="logo-menu" src="../img/logo-bar.png"></a>
                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
 
                 <ul class="menu right hide-on-med-and-down">
-                    <li><a href="aulas.php" class="thin"><i class="icon-menu material-icons">perm_identity</i>Aulas</a></li>
-                    <li><a href="midia.php" class="thin"><i class="icon-menu material-icons">play_circle_filled</i>Mídia</a></li>
-                    <li><a href="projetos.php" class="thin"><i class="icon-menu material-icons">work</i>Projetos</a></li>
+                    <li><a href="aulas" class="thin"><i class="icon-menu material-icons">perm_identity</i>Aulas</a></li>
+                    <li><a href="midia" class="thin"><i class="icon-menu material-icons">play_circle_filled</i>Mídia</a></li>
+                    <li><a href="projetos" class="thin"><i class="icon-menu material-icons">work</i>Projetos</a></li>
                 </ul>
 
                 <ul class="menu side-nav" id="mobile-demo">
-                    <li><a href="../index.php" class="thin white-text"><i class="icon-menu material-icons">store</i>Início</a></li>
-                    <li><a href="aulas.php" class="thin white-text"><i class="icon-menu material-icons">perm_identity</i>Aulas</a></li>
-                    <li><a href="midia.php" class="thin white-text"><i class="icon-menu material-icons">play_circle_filled</i>Mídia</a></li>
-                    <li><a href="projetos.php" class="thin white-text"><i class="icon-menu material-icons">work</i>Projetos</a></li>
+                    <li><a href="../index" class="thin white-text"><i class="icon-menu material-icons">store</i>Início</a></li>
+                    <li><a href="aulas" class="thin white-text"><i class="icon-menu material-icons">perm_identity</i>Aulas</a></li>
+                    <li><a href="midia" class="thin white-text"><i class="icon-menu material-icons">play_circle_filled</i>Mídia</a></li>
+                    <li><a href="projetos" class="thin white-text"><i class="icon-menu material-icons">work</i>Projetos</a></li>
                 </ul>
             </div>
         </nav>
--- pages/aulas.php
+++ pages/aulas.php
@@ -11,6 +11,7 @@
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Monkeys of Sampa - Aulas</title>
+    <link rel="icon" href="../img/favicon.ico">
 </head>
 
 <body>
